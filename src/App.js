a4ec1465d47bab08bdc6e2e55cd4746d2f9685b8
Lideqiang2019 <17805422918@163.com>
2020-11-10 21:30:36 +0800

    修改less全局颜色

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Button, message } from 'antd'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import './App.less';
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Admin} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,14 @@
+/*
+用户登陆的路由组件
+*/
+import React, { Component } from 'react'
+import {Button} from 'antd'
+export default class Login extends Component {
+    render() {
+        return (
+            <div>login
+                <Button type="primary">点击按钮</Button>
+            </div>
+        )
+    }
+}
