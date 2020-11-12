c2c9dc40e241592428d47069254ec5b9fb141254
Lideqiang2019 <17805422918@163.com>
2020-11-12 20:47:34 +0800

    左侧导航栏与路由

--- src/components/left-nav/left-nav.jsx
+++ src/components/left-nav/left-nav.jsx
@@ -1,51 +1,167 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { Menu, Button,Icon } from 'antd';
+import { Link,withRouter } from 'react-router-dom'
+import { Menu } from 'antd';
 import {
+    HomeOutlined,
     AppstoreOutlined,
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
+    BarsOutlined,
+    ToolOutlined,
+    UserOutlined,
+    SafetyOutlined,
+    AreaChartOutlined,
+    BarChartOutlined,
+    LineChartOutlined,
     PieChartOutlined,
-    DesktopOutlined,
-    ContainerOutlined,
-    MailOutlined,
+    WindowsOutlined
 } from '@ant-design/icons';
 import menuList from '../../config/menuConfig'
 import './left-nav.less'
 import logo from '../../assets/images/logo.png'
 const { SubMenu } = Menu;
 
-export default class LeftNav extends Component {
+class LeftNav extends Component {
     getMenuNodes = (menuList)=>{
-        return menuList.map(item=>{
+        return menuList.map((item)=>{
             if(!item.children){
-                return(
-                    <Menu.Item key={item.key}>
-                    <Link to={item.key}>
-                      <Icon type={item.icon}/>
-                      <span>{item.title}</span>
-                    </Link>
-                  </Menu.Item>
-                )
+                switch (item.icon) {
+                    case 'HomeOutlined':
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <HomeOutlined/>
+                              <span>{item.title}</span>
+                            </Link>
+                          </Menu.Item>
+                        )
+                    case 'BarsOutlined':
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <BarsOutlined/>
+                              <span>{item.title}</span>
+                            </Link>
+                          </Menu.Item>
+                        )
+                    case 'ToolOutlined':
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <ToolOutlined/>
+                                <span>{item.title}</span>
+                            </Link>
+                            </Menu.Item>
+                        )
+                    case 'UserOutlined':
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <UserOutlined/>
+                                <span>{item.title}</span>
+                            </Link>
+                            </Menu.Item>
+                        )
+                    case 'SafetyOutlined':
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <SafetyOutlined/>
+                                <span>{item.title}</span>
+                            </Link>
+                            </Menu.Item>
+                        )
+                        case 'BarChartOutlined':
+                            return(
+                                <Menu.Item key={item.key}>
+                                <Link to={item.key}>
+                                    <BarChartOutlined/>
+                                    <span>{item.title}</span>
+                                </Link>
+                                </Menu.Item>
+                            )
+                        case 'LineChartOutlined':
+                            return(
+                                <Menu.Item key={item.key}>
+                                <Link to={item.key}>
+                                    <LineChartOutlined/>
+                                    <span>{item.title}</span>
+                                </Link>
+                                </Menu.Item>
+                            )
+                        case 'PieChartOutlined':
+                            return(
+                                <Menu.Item key={item.key}>
+                                <Link to={item.key}>
+                                    <PieChartOutlined/>
+                                    <span>{item.title}</span>
+                                </Link>
+                                </Menu.Item>
+                            )
+                        
+                    default:
+                        return(
+                            <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <WindowsOutlined/>
+                              <span>{item.title}</span>
+                            </Link>
+                          </Menu.Item>
+                        )
+                }
+                
             }else{
-                return(
-                    <SubMenu
-                    key={item.key}
-                    title={
-                    <span>
-                        <Icon type={item.icon}/>
-                        <span>{item.title}</span>
-                    </span>
-                    }
-                >
-                    {this.getMenuNodes(item.children)}
-                </SubMenu>
-                )
+                // 查找一个与当前请求路径匹配的子Item
+                const pathname = this.props.location.pathname;
+                const cItem = item.children.find(cItem=>cItem.key===pathname)
+                // 如果存在, 说明当前item的子列表需要打开
+                if (cItem) {
+                    this.openKey = item.key
+                }
+                switch (item.icon) {
+                    case 'AppstoreOutlined':
+                        return(
+                            <SubMenu
+                            key={item.key}
+                            title={
+                            <span>
+                                <AppstoreOutlined />
+                                <span>{item.title}</span>
+                            </span>
+                            }
+                        >
+                            {this.getMenuNodes(item.children)}
+                        </SubMenu>
+                        )
+                    case 'AreaChartOutlined':
+                        return(
+                            <SubMenu
+                            key={item.key}
+                            title={
+                            <span>
+                                <AreaChartOutlined />
+                                <span>{item.title}</span>
+                            </span>
+                            }
+                        >
+                            {this.getMenuNodes(item.children)}
+                        </SubMenu>
+                        )
+                    default:
+                        break;
+                }
+                
             }
         })
     }
+    /*
+     在第一次render()之前执行一次
+     为第一个render()准备数据(必须同步的)
+   */
+    componentWillMount(){
+        this.menuNodes = this.getMenuNodes(menuList);
+    }
     render() {
-
+        const pathname = this.props.location.pathname;
+        const openKey = this.openKey;
         return (
             <div className='left-nav'>
                 <Link to='/' className='logo-link'>
@@ -53,8 +169,9 @@ export default class LeftNav extends Component {
                     <h1>后台服务</h1>
                 </Link>
                 <Menu
-                    defaultSelectedKeys={['1']}
-                    // defaultOpenKeys={['sub1']}
+                    // defaultSelectedKeys={['/home']}
+                    selectedKeys={[pathname]}
+                    defaultOpenKeys={[openKey]}
                     mode="inline"
                     theme="dark"
                 >
@@ -112,9 +229,11 @@ export default class LeftNav extends Component {
                     <Menu.Item key="9" icon={<PieChartOutlined />}>
                         <Link to="/order">订单管理</Link>
                     </Menu.Item> */}
-                    {this.getMenuNodes(menuList)}
+                    {this.menuNodes}
                 </Menu>
             </div>
         )
     }
 }
+
+export default withRouter(LeftNav)
