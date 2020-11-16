3100fff71c96eaa69142549c54d92f0e08e828a5
Lideqiang2019 <17805422918@163.com>
2020-11-16 16:12:09 +0800

    Category静态

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,14 +1,60 @@
 /*
-用户登陆的路由组件
+商品品类管理
 */
 import React, { Component } from 'react'
-
+import {Button,Card,Table} from 'antd'
+import {PlusOutlined} from '@ant-design/icons'
+import LinkButton from '../../components/link-button/link-button'
 export default class Category extends Component {
     render() {
+        const title = "一级分类列表"
+        const extra = (
+            <Button type='primary'>
+                <span>
+                    <PlusOutlined />
+                    添加
+                </span>
+            </Button>
+        )
+
+        const dataSource = [
+            {
+              key: '1',
+              name: '胡彦斌',
+            
+            },
+          ];
+          
+          const columns = [
+            {
+              title: '分类的名称',
+              dataIndex: 'name',
+              key: 'name',
+              width:'75%'
+            },
+            {
+                title: '操作',
+                dataIndex: '',
+                key: 'x',
+                render:()=>(
+                  <span>
+                      <LinkButton>修改分类</LinkButton>
+                      <LinkButton>查看子分类</LinkButton>
+                  </span>
+              )
+              },
+           
+          ];
+
         return (
-            <div >
-                Category
-            </div>
+            <Card title={title} extra={extra}>
+                <Table 
+                dataSource={dataSource}
+                columns={columns}
+                bordered
+                />;
+            </Card>
+           
         )
     }
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,12 +2,13 @@
 用户登陆的路由组件
 */
 import React, { Component } from 'react'
+import './index.less'
 
 export default class Home extends Component {
     render() {
         return (
-            <div >
-                Home
+            <div className='home'>
+                欢迎来到Qlab！
             </div>
         )
     }
